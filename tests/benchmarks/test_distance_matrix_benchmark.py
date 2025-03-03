564ae505b93965f85ce68337cc1479ed11320d81
Andres <andres.sandoval@alumnos.usm.cl>
2025-03-03 11:23:22 -0300

    Update test

--- tests/benchmarks/test_distance_matrix_benchmark.py
+++ tests/benchmarks/test_distance_matrix_benchmark.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 import numpy as np
 from scipy.spatial.distance import cdist  # type: ignore
@@ -8,11 +10,14 @@ from pymoors._pymoors import cross_euclidean_distances  # type: ignore
 @pytest.mark.parametrize("n", [100, 500, 1000, 2000])
 def test_compare_scipy_cdist_vs_pymoors(benchmark, n):
     matrix = np.random.rand(n, n)
-
-    result_pymoors = benchmark(cross_euclidean_distances(matrix, matrix))
-    result_scipy = benchmark(cdist(matrix, matrix, metric="sqeuclidean"))
-
-    mean_f1 = result_pymoors.stats.mean
-    mean_f2 = result_scipy.stats.mean
-
-    assert mean_f1 < mean_f2
+    result_pymoors = benchmark(cross_euclidean_distances, matrix, matrix)
+    # Measure the time for scipy's cdist using a timer
+    start_time = time.perf_counter()
+    result_scipy = cdist(matrix, matrix, metric="sqeuclidean")
+    end_time = time.perf_counter()
+    time_scipy = end_time - start_time
+    # Check the result is the same
+    np.testing.assert_allclose(result_pymoors, result_scipy, rtol=1e-5, atol=1e-8)
+    # Check the time
+    mean_time_pymoors = benchmark.stats.stats.mean
+    assert mean_time_pymoors < time_scipy
